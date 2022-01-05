ba0299140ccba0c4a0f105d197a8c78a90ea3987
Gurnehmat K. Dhindsa <gurnehmat.dhindsa@gmail.com>
2022-01-05 17:44:56 +0530

    logout

--- backend/src/routers/users.js
+++ backend/src/routers/users.js
@@ -97,5 +97,19 @@ router.post('/users/login', async(req,res)=>{
        res.status(400).send() 
    }
 })
+//Logout
+router.post('/users/logout', auth, async(req,res)=>{
+    try {
+        console.log(req.token)
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
 
 module.exports=router
